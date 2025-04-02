60025c3abfae3cc059425874cd2cd45d908a1436
Samira Noor <samirazein20@gmail.com>
2025-04-02 17:22:50 -0500

    Load products from backend

--- data/products.js
+++ data/products.js
@@ -56,6 +56,30 @@ class Clothing extends Product {
   }
 }
 
+
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product (productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -723,3 +747,4 @@ export const products = [
 });
 
 console.log(products)
+*/

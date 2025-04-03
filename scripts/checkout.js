d767f6378d11401e0fd73b9ae36e1a5b186c068f
Samira Noor <samirazein20@gmail.com>
2025-04-03 15:00:51 -0500

    Lern promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,48 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    console.log('Start promise');
+    loadProducts(() => {
+      console.log('Finished loading');
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(()=>{
+      resolve();
+    });
+  })
+  
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+new Promise((resolve) => {
+  console.log('Start promise');
+  loadProducts(() => {
+    console.log('Finished loading');
+    resolve();
+  });
+}).then(() => {
+  console.log('Next step!!!');
+  return new Promise((resolve) => {
+    loadCart(()=>{
+      resolve();
+    });
+  });
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   })
+// });
